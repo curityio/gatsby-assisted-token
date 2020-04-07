ba39bc9202213fe655cc595bb28b8aba91ab5146
Daniel Lindau <d.lindau@gmail.com>
2020-04-07 10:28:43 +0200

    Initial functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Assisted Token Example`,
+    description: `An example of using the Assisted Token flow with a default Gatsby.js application.`,
+    author: `Daniel Lindau, Curity AB`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
